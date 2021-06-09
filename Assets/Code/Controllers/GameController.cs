d1863c58273bba04625befffba52e3849d70e072
AlekTananykin <tananykin@mail.ru>
2021-06-10 00:10:21 +0300

    charecter view

---
+++ Assets/Code/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private SpriteRenderer _back;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
