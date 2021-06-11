9ea357833d3e2ca7ca6d204f7edf4321b4a13792
AlekTananykin <tananykin@mail.ru>
2021-06-12 01:02:31 +0300

    ogre sprites arte added

---
+++ Assets/Code/Controllers/OgreController.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Code.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Controllers
+{
+    class OgreController : IExecute, IInitialization
+    {
+        public void Execute(float deltaTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
